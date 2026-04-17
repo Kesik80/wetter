43eb98328b6f573d56dd49f0a0cd6f807e62be7f
Kesik80 <55840881+Kesik80@users.noreply.github.com>
2026-04-17 22:27:08 +0200

    Upload 1 file(s)

--- api/tts.js
+++ api/tts.js
@@ -45,7 +45,7 @@ export default async function handler(req, res) {
     }
 
     // Генерируем через ElevenLabs - СТАНДАРТНЫЙ ГОЛОС (бесплатный)
-    const voiceId = '7eVMgwCnXydb3CikjV7a'; // Bella
+    const voiceId = 'AnvlJBAqSLDzEevYr9Ap'; // Bella
     const elevenRes = await fetch(`https://api.elevenlabs.io/v1/text-to-speech/${voiceId}`, {
       method: 'POST',
       headers: {
